bcf5e6fa79abe9e281f19288cbf7d71ca567178b
yfsoftcom <yfsoftcom@126.com>
2017-02-05 13:15:49 +0800

    add mysql dbm

--- M.js
+++ M.js
@@ -1,5 +1,5 @@
 import Promise from 'bluebird';
 import DBM from 'yf-fpm-dbm';
-import mysqlOptions from 'mysqlOptions.js'
+import mysqlOptions from './mysqlOptions.js'
 let M = Promise.promisifyAll(DBM(mysqlOptions));
 export default M;
