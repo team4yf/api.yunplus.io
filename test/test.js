7247f19ac3e8241b29622344f5d34943e28f6aa6
yfsoftcom <yfsoftcom@126.com>
2017-02-22 00:00:50 +0800

    add baidu

--- test/test.js
+++ test/test.js
@@ -33,9 +33,34 @@ import fetchData from './fetch'
 //   })
 
 
-fetchData('system.sendMail', { templateId: 3 , to: '1794947912@qq.com', subject: 'test', data: {
-  foo: 'test that'
-}})
+// fetchData('system.sendMail', { templateId: 3 , to: '1794947912@qq.com', subject: 'test', data: {
+//   foo: 'test that'
+// }})
+//   .then((json) => {
+//     console.log(json)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+
+// fetchData('schedule.start', {method: 'system.show', v: '0.0.1', cron: '* * * * *'})
+//   .then((json) => {
+//     console.log(json)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+// fetchData('schedule.stop', {jobId: 0})
+//   .then((json) => {
+//     console.log(json)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+fetchData('baidu.check', {})
   .then((json) => {
     console.log(json)
   })
