63f74eccf1fa86873adf299986f078f4428f5a99
yfsoftcom <yfsoftcom@126.com>
2018-05-31 15:00:05 +0800

    remove unused test scripts

--- test/command.test.js
+++ test/command.test.js
@@ -1,11 +1,23 @@
-var should = require("should");
-var YF = require("yf-fpm-client-nodejs").default;
-YF.init({ appkey:'123123', masterKey:'1b7e5703602b6fce1cae7364ac0f2244', endpoint: 'http://localhost:9991/api' });
+var should = require("chai").should;
+var fpmc = require("yf-fpm-client-js").default;
+fpmc.init({ mode: 'DEV', appkey:'123123', masterKey:'123123', domain: 'http://localhost:9999' });
 
-describe('Command', function(){
-  it('doCommand', function(done){
-    var func = new YF.Func('system.doCommand');
-    func.invoke({ command: 'dir d:'})
+describe('Function', function(){
+  it('test', function(done){
+    var func = new fpmc.Func('test.foo');
+    func.invoke({})
+      .then(function(d){
+        console.log(d)
+        done();
+      }).catch(function(err){
+        done(err);
+      });
+  
+  })
+
+  it('test ExtendModule', function(done){
+    var func = new fpmc.Func('demo.foo');
+    func.invoke({})
       .then(function(d){
         console.log(d)
         done();
